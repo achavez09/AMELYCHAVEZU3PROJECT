f9ab4ff3c436cac7cdc8bc14b3aea4078832ea5d
Amely Chavez <139826542+achavez09@users.noreply.github.com>
2024-03-25 20:37:52 +0000

    Declare variables.

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
-
-
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionTwoScreen = document.querySelector(".option-two-screen");
+let optionOne = document.querySelector(".option-one");
+let optionTwo = document.querySelector(".option-two");
+let optionTwoEnd = document.querySelector(".option-two-end");
+let optionThree = document.querySelector(".option-three");
+let optionThreeEnd = document.querySelector(".option-three-end");
+let optionFour = document.querySelector(".option-four");
+let optionFourEnd = document.querySelector(".option-four-end");
+let buttons = document.querySelector(".buttons");
 
 
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
+optionOne.addEventListener('click', function(){
+  optionOneScreen.style = "visible";
+  optionThree.style = "visible";
+   optionFour.style = "visible";
+)};
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+buttons.optionTwo.addEventListener('click', function(){
+  optionTwoScreen.style.display = "visible";
 
-// });
+});
 
+optionTwoScreen.addEventListener('dblclick', function(){
+  optionTwoEnd.style.display = "visible";
+
+});
 // INSERT_VARIABLE.addEventListener('click', function(){
 
 // });
